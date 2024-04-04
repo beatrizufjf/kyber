d056068421ac3c9fae8e0bfa88eb387af0ec5d34
beatrizufjf <beatriz.azevedo@estudante.ufjf.br>
2024-04-04 15:01:43 +0000

    first commit

---
+++ NIST-PQ-Submission-Kyber-20201001/Optimized_Implementation/crypto_kem/kyber512/forro_cipher-main/src/forro/ref/forro_prf.cpp
@@ -0,0 +1,13 @@
+#include "forro.h"
+
+
+void forro_prf(uint8_t *out, size_t outlen, const uint8_t key[KYBER_SYMBYTES], uint8_t nonce)
+{
+    stream_ctx ctx = {0};
+    forro_keysetup(&ctx, &key);
+    // forro_keysetup(stream_ctx *x, uint8_t *key);
+    uint8_t iv[16] = { 0 };
+    iv[0] = nonce;
+    forro_ivsetup(&ctx, iv);
+    // forro_ivsetup(stream_ctx *x, uint8_t *iv);
+}
