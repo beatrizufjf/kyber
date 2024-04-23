c11edc04e556d414c1d64294577e27908c195d45
beatrizufjf <beatriz.azevedo@estudante.ufjf.br>
2024-04-23 07:41:45 +0000

    execução de ajuste na PRF

--- NIST-PQ-Submission-Kyber-20201001/Optimized_Implementation/crypto_kem/kyber512/forro_cipher-main/src/forro/ref/forro_prf.cpp
+++ NIST-PQ-Submission-Kyber-20201001/Optimized_Implementation/crypto_kem/kyber512/forro_cipher-main/src/forro/ref/forro_prf.cpp
@@ -10,4 +10,5 @@ void forro_prf(uint8_t *out, size_t outlen, const uint8_t key[KYBER_SYMBYTES], u
     iv[0] = nonce;
     forro_ivsetup(&ctx, iv);
     // forro_ivsetup(stream_ctx *x, uint8_t *iv);
+    forro_keystream_bytes(&ctx, out, (uint32_t)outlen);
 }
