ccdbbfc6a9e0cd7e63aa3dea49828b09a5646bf2
11962 <vinicius.1232@abin.gov.br>
2024-05-06 18:00:47 -0300

    feat: codigo de plot otimizado

--- plot/main.py
+++ plot/main.py
@@ -5,172 +5,29 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import time
 
-############## K=2 ################
-df_aes_k2 = pd.read_csv('../NIST-PQ-Submission-Kyber-20201001/Optimized_Implementation/crypto_kem/kyber512/results/RESULTS_AES-K2.csv')
-df_shake_k2 = pd.read_csv('../NIST-PQ-Submission-Kyber-20201001/Optimized_Implementation/crypto_kem/kyber512/results/RESULTS_SHAKE-K2.csv')
-df_forro_k2 = pd.read_csv('../NIST-PQ-Submission-Kyber-20201001/Optimized_Implementation/crypto_kem/kyber512/results/RESULTS_FORRO-K2.csv')
-df_xote_k2 = pd.read_csv('../NIST-PQ-Submission-Kyber-20201001/Optimized_Implementation/crypto_kem/kyber512/results/RESULTS_XOTE-K2.csv')
+folderpath = "figures/"
+figures_format = "png"
+# figures_format = "eps"
 
-# Plot list gen_a
-# plot_list = [df_aes_k2['gen_a'],df_shake_k2['gen_a'],df_forro_k2['gen_a'],df_xote_k2['gen_a']]
-plot_list = [df_shake_k2['gen_a'],df_forro_k2['gen_a'],df_xote_k2['gen_a']]
-plt.figure(figsize =(11, 6))
-bplots = plt.boxplot(plot_list,  vert = 1, patch_artist = False)
-# Adicionando Título ao gráfico
-plt.title("Boxplot gen_a", loc="center", fontsize=18)
-# plt.xticks([1, 2, 3, 4], ['AES', 'SHAKE', 'FORRO', 'XOTE'])
-plt.xticks([1, 2, 3], ['SHAKE', 'FORRO', 'XOTE'])
-plt.xlabel("Algorithm")
-plt.ylabel("Cycles/ticks")
-plt.show()
+all_algs = ['AES', 'SHAKE', 'FORRO', 'XOTE']
+print_algs = ['SHAKE', 'XOTE']
+list_count = list(range(1, len(print_algs)+1))
+print(list_count)
 
-# Plot list keypair
-# plot_list = [df_aes_k2['keypair'],df_shake_k2['keypair'],df_forro_k2['keypair'],df_xote_k2['keypair']]
-plot_list = [df_shake_k2['keypair'],df_forro_k2['keypair'],df_xote_k2['keypair']]
-plt.figure(figsize =(11, 6))
-bplots = plt.boxplot(plot_list,  vert = 1, patch_artist = False)
-# Adicionando Título ao gráfico
-plt.title("Boxplot Keygen", loc="center", fontsize=18)
-# plt.xticks([1, 2, 3, 4], ['AES', 'SHAKE', 'FORRO', 'XOTE'])
-plt.xticks([1, 2, 3], ['SHAKE', 'FORRO', 'XOTE'])
-plt.xlabel("Algorithm")
-plt.ylabel("Cycles/ticks")
-plt.show()
-
-# plot_list = [df_aes_k2['encaps'],df_shake_k2['encaps'],df_forro_k2['encaps'],df_xote_k2['encaps']]
-plot_list = [df_shake_k2['encaps'],df_forro_k2['encaps'],df_xote_k2['encaps']]
-plt.figure(figsize =(11, 6))
-bplots = plt.boxplot(plot_list,  vert = 1, patch_artist = False)
-# Adicionando Título ao gráfico
-plt.title("Boxplot Encaps", loc="center", fontsize=18)
-# plt.xticks([1, 2, 3, 4], ['AES', 'SHAKE', 'FORRO', 'XOTE'])
-plt.xticks([1, 2, 3], ['SHAKE', 'FORRO', 'XOTE'])
-plt.xlabel("Algorithm")
-plt.ylabel("Cycles/ticks")
-plt.show()
-
-# plot_list = [df_aes_k2['decaps'],df_shake_k2['decaps'],df_forro_k2['decaps'],df_xote_k2['decaps']]
-plot_list = [df_shake_k2['decaps'],df_forro_k2['decaps'],df_xote_k2['decaps']]
-plt.figure(figsize =(11, 6))
-bplots = plt.boxplot(plot_list,  vert = 1, patch_artist = False)
-# Adicionando Título ao gráfico
-plt.title("Boxplot Decaps", loc="center", fontsize=18)
-# plt.xticks([1, 2, 3, 4], ['AES', 'SHAKE', 'FORRO', 'XOTE'])
-plt.xticks([1, 2, 3], ['SHAKE', 'FORRO', 'XOTE'])
-plt.xlabel("Algorithm")
-plt.ylabel("Cycles/ticks")
-plt.show()
-
-
-############## K=3 ################
-df_aes_k3 = pd.read_csv('../NIST-PQ-Submission-Kyber-20201001/Optimized_Implementation/crypto_kem/kyber512/results/RESULTS_AES-K3.csv')
-df_shake_k3 = pd.read_csv('../NIST-PQ-Submission-Kyber-20201001/Optimized_Implementation/crypto_kem/kyber512/results/RESULTS_SHAKE-K3.csv')
-df_forro_k3 = pd.read_csv('../NIST-PQ-Submission-Kyber-20201001/Optimized_Implementation/crypto_kem/kyber512/results/RESULTS_FORRO-K3.csv')
-df_xote_k3 = pd.read_csv('../NIST-PQ-Submission-Kyber-20201001/Optimized_Implementation/crypto_kem/kyber512/results/RESULTS_XOTE-K3.csv')
-
-# Plot list gen_a
-# plot_list = [df_aes_k3['gen_a'],df_shake_k3['gen_a'],df_forro_k3['gen_a'],df_xote_k3['gen_a']]
-plot_list = [df_shake_k3['gen_a'],df_forro_k3['gen_a'],df_xote_k3['gen_a']]
-plt.figure(figsize =(11, 6))
-bplots = plt.boxplot(plot_list,  vert = 1, patch_artist = False)
-# Adicionando Título ao gráfico
-plt.title("Boxplot gen_a", loc="center", fontsize=18)
-# plt.xticks([1, 2, 3, 4], ['AES', 'SHAKE', 'FORRO', 'XOTE'])
-plt.xticks([1, 2, 3], ['SHAKE', 'FORRO', 'XOTE'])
-plt.xlabel("Algorithm")
-plt.ylabel("Cycles/ticks")
-plt.show()
-
-# Plot list keypair
-# plot_list = [df_aes_k3['keypair'],df_shake_k3['keypair'],df_forro_k3['keypair'],df_xote_k3['keypair']]
-plot_list = [df_shake_k3['keypair'],df_forro_k3['keypair'],df_xote_k3['keypair']]
-plt.figure(figsize =(11, 6))
-bplots = plt.boxplot(plot_list,  vert = 1, patch_artist = False)
-# Adicionando Título ao gráfico
-plt.title("Boxplot Keygen", loc="center", fontsize=18)
-# plt.xticks([1, 2, 3, 4], ['AES', 'SHAKE', 'FORRO', 'XOTE'])
-plt.xticks([1, 2, 3], ['SHAKE', 'FORRO', 'XOTE'])
-plt.xlabel("Algorithm")
-plt.ylabel("Cycles/ticks")
-plt.show()
-
-# plot_list = [df_aes_k3['encaps'],df_shake_k3['encaps'],df_forro_k3['encaps'],df_xote_k3['encaps']]
-plot_list = [df_shake_k3['encaps'],df_forro_k3['encaps'],df_xote_k3['encaps']]
-plt.figure(figsize =(11, 6))
-bplots = plt.boxplot(plot_list,  vert = 1, patch_artist = False)
-# Adicionando Título ao gráfico
-plt.title("Boxplot Encaps", loc="center", fontsize=18)
-# plt.xticks([1, 2, 3, 4], ['AES', 'SHAKE', 'FORRO', 'XOTE'])
-plt.xticks([1, 2, 3], ['SHAKE', 'FORRO', 'XOTE'])
-plt.xlabel("Algorithm")
-plt.ylabel("Cycles/ticks")
-plt.show()
-
-# plot_list = [df_aes_k3['decaps'],df_shake_k3['decaps'],df_forro_k3['decaps'],df_xote_k3['decaps']]
-plot_list = [df_shake_k3['decaps'],df_forro_k3['decaps'],df_xote_k3['decaps']]
-plt.figure(figsize =(11, 6))
-bplots = plt.boxplot(plot_list,  vert = 1, patch_artist = False)
-# Adicionando Título ao gráfico
-plt.title("Boxplot Decaps", loc="center", fontsize=18)
-# plt.xticks([1, 2, 3, 4], ['AES', 'SHAKE', 'FORRO', 'XOTE'])
-plt.xticks([1, 2, 3], ['SHAKE', 'FORRO', 'XOTE'])
-plt.xlabel("Algorithm")
-plt.ylabel("Cycles/ticks")
-plt.show()
-
-
-############## K=4 ################
-df_aes_k4 = pd.read_csv('../NIST-PQ-Submission-Kyber-20201001/Optimized_Implementation/crypto_kem/kyber512/results/RESULTS_AES-K4.csv')
-df_shake_k4 = pd.read_csv('../NIST-PQ-Submission-Kyber-20201001/Optimized_Implementation/crypto_kem/kyber512/results/RESULTS_SHAKE-K4.csv')
-df_forro_k4 = pd.read_csv('../NIST-PQ-Submission-Kyber-20201001/Optimized_Implementation/crypto_kem/kyber512/results/RESULTS_FORRO-K4.csv')
-df_xote_k4 = pd.read_csv('../NIST-PQ-Submission-Kyber-20201001/Optimized_Implementation/crypto_kem/kyber512/results/RESULTS_XOTE-K4.csv')
-
-# Plot list gen_a
-# plot_list = [df_aes_k4['gen_a'],df_shake_k4['gen_a'],df_forro_k4['gen_a'],df_xote_k4['gen_a']]
-plot_list = [df_shake_k4['gen_a'],df_forro_k4['gen_a'],df_xote_k4['gen_a']]
-plt.figure(figsize =(11, 6))
-bplots = plt.boxplot(plot_list,  vert = 1, patch_artist = False)
-# Adicionando Título ao gráfico
-plt.title("Boxplot gen_a", loc="center", fontsize=18)
-# plt.xticks([1, 2, 3, 4], ['AES', 'SHAKE', 'FORRO', 'XOTE'])
-plt.xticks([1, 2, 3], ['SHAKE', 'FORRO', 'XOTE'])
-plt.xlabel("Algorithm")
-plt.ylabel("Cycles/ticks")
-plt.show()
-
-# Plot list keypair
-# plot_list = [df_aes_k4['keypair'],df_shake_k4['keypair'],df_forro_k4['keypair'],df_xote_k4['keypair']]
-plot_list = [df_shake_k4['keypair'],df_forro_k4['keypair'],df_xote_k4['keypair']]
-plt.figure(figsize =(11, 6))
-bplots = plt.boxplot(plot_list,  vert = 1, patch_artist = False)
-# Adicionando Título ao gráfico
-plt.title("Boxplot Keygen", loc="center", fontsize=18)
-# plt.xticks([1, 2, 3, 4], ['AES', 'SHAKE', 'FORRO', 'XOTE'])
-plt.xticks([1, 2, 3], ['SHAKE', 'FORRO', 'XOTE'])
-plt.xlabel("Algorithm")
-plt.ylabel("Cycles/ticks")
-plt.show()
-
-# plot_list = [df_aes_k4['encaps'],df_shake_k4['encaps'],df_forro_k4['encaps'],df_xote_k4['encaps']]
-plot_list = [df_shake_k4['encaps'],df_forro_k4['encaps'],df_xote_k4['encaps']]
-plt.figure(figsize =(11, 6))
-bplots = plt.boxplot(plot_list,  vert = 1, patch_artist = False)
-# Adicionando Título ao gráfico
-plt.title("Boxplot Encaps", loc="center", fontsize=18)
-# plt.xticks([1, 2, 3, 4], ['AES', 'SHAKE', 'FORRO', 'XOTE'])
-plt.xticks([1, 2, 3], ['SHAKE', 'FORRO', 'XOTE'])
-plt.xlabel("Algorithm")
-plt.ylabel("Cycles/ticks")
-plt.show()
-
-# plot_list = [df_aes_k4['decaps'],df_shake_k4['decaps'],df_forro_k4['decaps'],df_xote_k4['decaps']]
-plot_list = [df_shake_k4['decaps'],df_forro_k4['decaps'],df_xote_k4['decaps']]
-plt.figure(figsize =(11, 6))
-bplots = plt.boxplot(plot_list,  vert = 1, patch_artist = False)
-# Adicionando Título ao gráfico
-plt.title("Boxplot Decaps", loc="center", fontsize=18)
-# plt.xticks([1, 2, 3, 4], ['AES', 'SHAKE', 'FORRO', 'XOTE'])
-plt.xticks([1, 2, 3], ['SHAKE', 'FORRO', 'XOTE'])
-plt.xlabel("Algorithm")
-plt.ylabel("Cycles/ticks")
-plt.show()
+for k in [2, 3, 4]:
+    print(f'Plotting k = {k}')
+    for func in ['gen_a', 'keypair', 'encaps', 'decaps']:
+        plot_list = []
+        for cur_alg in all_algs:
+            if(cur_alg in print_algs):
+                df = pd.read_csv('../NIST-PQ-Submission-Kyber-20201001/Optimized_Implementation/crypto_kem/kyber512/results/RESULTS_' + cur_alg + '-K' + str(k) + '.csv')
+                plot_list.append(df[func])
+        plt.figure(figsize =(11, 6))
+        bplots = plt.boxplot(plot_list, vert = 1, patch_artist = False, labels=print_algs)
+        # Adicionando Título ao gráfico
+        plt.title("Boxplot " + func, loc="center", fontsize=18)
+        # plt.xticks(list_count, print_algs)
+        plt.xlabel("Algorithm")
+        plt.ylabel("Cycles/ticks")
+        plt.savefig(folderpath + func + '-K' + str(k) + '.' + figures_format, format=figures_format)
+        # plt.show()
